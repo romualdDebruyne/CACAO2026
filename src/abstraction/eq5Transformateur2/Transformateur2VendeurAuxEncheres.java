19308c0bd15685b8daba6f4b94db64df5ead5b3d
Maxence Lepetit <maxence.lepetit@imt-atlantique.net>
2026-03-10 10:40:46 +0100

    VendeurEnchereV1

--- src/abstraction/eq5Transformateur2/Transformateur2VendeurAuxEncheres.java
+++ src/abstraction/eq5Transformateur2/Transformateur2VendeurAuxEncheres.java
@@ -10,7 +10,8 @@ import abstraction.eqXRomu.filiere.Filiere;
 import abstraction.eqXRomu.filiere.IActeur;
 import abstraction.eqXRomu.general.Journal;
 import abstraction.eqXRomu.general.Variable;
-import abstraction.eqXRomu.produits.IProduit; 
+import abstraction.eqXRomu.produits.IProduit;
+import abstraction.eqXRomu.produits.Chocolat; 
 /**
  * @author Maxence
  */
@@ -23,11 +24,30 @@ public class Transformateur2VendeurAuxEncheres extends Transformateur2AcheteurBo
 
 
     public void VendreEncheres(){
+        Double c_bq=this.getStock_chocolat(Chocolat.C_BQ);
+        Double c_bq_e=this.getStock_chocolat(Chocolat.C_BQ_E);
+        Double c_mq=this.getStock_chocolat(Chocolat.C_MQ);
+        Double c_mq_e=this.getStock_chocolat(Chocolat.C_MQ_E);
+        Double c_hq=this.getStock_chocolat(Chocolat.C_HQ);
+        Double c_hq_e=this.getStock_chocolat(Chocolat.C_HQ_E);
+
+        superviseur.vendreAuxEncheres(this, cryptogramme, Chocolat.C_BQ, c_bq);
+        superviseur.vendreAuxEncheres(this, cryptogramme, Chocolat.C_BQ_E, c_bq_e);
+        superviseur.vendreAuxEncheres(this, cryptogramme, Chocolat.C_MQ, c_mq);
+        superviseur.vendreAuxEncheres(this, cryptogramme, Chocolat.C_MQ_E, c_mq_e);
+        superviseur.vendreAuxEncheres(this, cryptogramme, Chocolat.C_HQ, c_hq);
+        superviseur.vendreAuxEncheres(this, cryptogramme, Chocolat.C_HQ, c_hq_e);
     }
 
     public Enchere choisir(List<Enchere> propositions) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'choisir'");
+        Enchere choisie=propositions.getFirst();
+        for (Enchere enchere : propositions) {
+            if(enchere.getPrixTonne()<choisie.getPrixTonne()){
+                choisie=enchere;
+            }}
+            return choisie;
+        }
+    
+
     }
     
-}
