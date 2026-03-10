e76c5f7b7f80f959fdeed12c11a90a023d7d500d
unknown <matteo.touzet@imt-atlantique.net>
2026-03-10 11:53:31 +0100

    Matteo TOUZET: Correction de la gestion de la file d'actions de la classe stock + complément des fonctions remove dans le cas ou le stock est insuffisant

--- src/abstraction/eq7Transformateur4/StockEq7.java
+++ src/abstraction/eq7Transformateur4/StockEq7.java
@@ -42,7 +42,7 @@ public class StockEq7 {
     }
 
     public String toString(){
-        return "Nom du propriétaire du stock : " + this.getCreateur().getNom() + ". Stock de bass qualité : " + Double.toString(this.getLowQ()) + ". Stock de moyenne qualité : " + Double.toString(this.getMedQ()) + ". Stock de haute qualité : " + Double.toString(this.getHighQ())  ;
+        return "Nom du propriétaire du stock : " + this.getCreateur().getNom() + ". Stock de basse qualité : " + Double.toString(this.getLowQ()) + ". Stock de moyenne qualité : " + Double.toString(this.getMedQ()) + ". Stock de haute qualité : " + Double.toString(this.getHighQ())  ;
     }
 
     public void AddLowQ(double valeur){
@@ -68,7 +68,6 @@ public class StockEq7 {
                 if (this.actions.get(i).getQuantity()<quantity){
                     quantity=quantity - this.actions.get(i).getQuantity();
                     this.actions.remove(i);
-                    i++;
                 }
                 else{
                     this.actions.get(i).RemoveQuant(quantity);
@@ -82,18 +81,34 @@ public class StockEq7 {
     }
 
     public void RemoveLowQ(double valeur){
-        this.LowQ.retirer(this.createur, valeur);
-        this.RemoveAction(Gamme.BQ, valeur);
+        if (valeur<this.getLowQ()){
+            this.LowQ.retirer(this.createur, valeur);
+            this.RemoveAction(Gamme.BQ, valeur);
+        }
+        else {
+            System.out.println("Stock de fèves BQ insuffisant");
+        }
     }
 
     public void RemoveMedQ(double valeur){
-        this.MedQ.retirer(this.createur, valeur);
-        this.RemoveAction(Gamme.MQ, valeur);
+        if (valeur<this.getMedQ()){
+            this.MedQ.retirer(this.createur, valeur);
+            this.RemoveAction(Gamme.MQ, valeur);
+        }
+        else{
+            System.out.println("Stock de fèves MQ insuffisant");
+        }
     }
 
+
     public void RemoveHighQ(double valeur){
+        if (valeur<this.getHighQ()){
         this.HighQ.retirer(this.createur, valeur);
         this.RemoveAction(Gamme.HQ, valeur);
+        }
+        else{
+            System.out.println("Stock de fèves HQ insuffisant");
+        }
     }
 
     public void SetLowQ(double valeur){
@@ -137,14 +152,29 @@ public class StockEq7 {
             }
         }
     }
+    /* 
     public static void main(String[] args) {
         IActeur acteur = new Transformateur4Acteur();
         StockEq7 stock = new StockEq7(acteur);
         stock.AddHighQ(90.);
         System.out.println(stock.actions.toString());
         System.out.println(stock.toString());
+
         stock.next();
-        stock.toString();
+        stock.AddHighQ(80.);
+        System.out.println(stock.toString());
+
+        stock.next();
+        stock.RemoveHighQ(40.);
+        System.out.println(stock.actions.toString());
+        System.out.println(stock.toString());
+
+        stock.next();
+        stock.RemoveHighQ( 70.);
+        System.out.println(stock.actions.toString());
+        System.out.println(stock.toString());
+
+        
         for (int i=0; i<6; i++){
             stock.next();
         }
@@ -153,8 +183,10 @@ public class StockEq7 {
         for (int i=0; i<12; i++){
             stock.next();
         }
+        stock.RemoveHighQ(50.);
         System.out.println(stock.toString());
         System.out.println(stock.actions.toString());
     }
+        */
 
 }
