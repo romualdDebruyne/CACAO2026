6e39e10e3769b71d6f240c5c6935467ff1aff476
Théophile Trillat <theophiletrillat@gmail.com>
2026-03-10 10:31:42 +0100

    Enchères

--- src/abstraction/eq1Producteur1/Producteur1AcheteurBourse.java
+++ src/abstraction/eq1Producteur1/Producteur1AcheteurBourse.java
@@ -3,27 +3,53 @@ package abstraction.eq1Producteur1;
 import java.util.ArrayList;
 import java.util.List;
 
-import abstraction.eqXRomu.encheres.Enchere;
-import abstraction.eqXRomu.encheres.IAcheteurAuxEncheres;
-import abstraction.eqXRomu.encheres.MiseAuxEncheres;
+import abstraction.eqXRomu.produits.Gamme;
+import abstraction.eqXRomu.bourseCacao.IAcheteurBourse;
+import abstraction.eqXRomu.filiere.Filiere;
+import abstraction.eqXRomu.produits.Feve;
 
-/** 
- * @author Elise Dossal
+/**
+ * @author Théophile Trillat & Elise Dossal
  */
+public class Producteur1AcheteurBourse extends Producteur1Acteur implements IAcheteurBourse {
 
-public class Producteur1AcheteurBourse extends Producteur1Acteur {//} implements IAcheteurAuxEncheres{
+    private double achatMaxParStep;
 
-
-    public Producteur1AcheteurBourse(){
+    public Producteur1AcheteurBourse() {
         super();
-
     }
 
-    public double proposerPrix(MiseAuxEncheres miseAuxEncheres){
+    public Producteur1AcheteurBourse(Feve feve, double stock, double achatMaxParStep) {
+        super(feve, stock);
+        this.achatMaxParStep = achatMaxParStep;
+    }
+
+    @Override
+    public double proposerPrix(MiseAuxEncheres miseAuxEncheres) {
         return 0.0;
-        
-
     }
 
+    @Override
+    public double demande(Feve f, double cours) {
+        if (this.getFeve().equals(f)) {
+            BourseCacao bourse = (BourseCacao) Filiere.LA_FILIERE.getActeur("BourseCacao");
+            double max    = bourse.getCours(getFeve()).getMax();
+            double min    = bourse.getCours(getFeve()).getMin();
+            double valeur = bourse.getCours(getFeve()).getValeur();
+            double pourcentage = (max - valeur) / (max - min);
+            return achatMaxParStep * pourcentage;
+        }
+        return 0.0;
+    }
 
+    @Override
+    public void notificationAchat(Feve f, double quantiteEnT, double coursEnEuroParT) {
+        this.stockFeve.setValeur(this, this.stockFeve.getValeur() + quantiteEnT);
+    }
+
+    @Override
+    public void notificationBlackList(int dureeEnStep) {
+        this.journal.ajouter("Aie... je suis blackliste... j'aurais du verifier "
+                + "que j'avais assez d'argent avant de passer une trop grosse commande en bourse...");
+    }
 }
