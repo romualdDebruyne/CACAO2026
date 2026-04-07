6dba52c3ddaec2cc2928351ed865c07d3b4997b3
Pierre Guttierez <pierre.guttierez@imt-atlantique.net>
2026-04-07 10:52:06 +0200

    V1 Next

--- src/abstraction/eq5Transformateur2/Transformateur2Acteur.java
+++ src/abstraction/eq5Transformateur2/Transformateur2Acteur.java
@@ -69,6 +69,17 @@ public class Transformateur2Acteur implements IActeur {
 		for (int i = 0; i < 9; i++){
 			this.Journaux.get(i).ajouter("Etape : "+ Integer.toString((Filiere.LA_FILIERE.getEtape()))+ "\n");
 		}
+		if achat enchere bien
+		if achat bourse bien
+		if stock feve ok vente enchere
+		if vente AO on propose
+		
+		// Vision plus long terme pour faire baisser les prix
+
+		if CC feve
+		if CC chococlat
+
+		faire du stock de choco
 	}
 
 	public Color getColor() {// NE PAS MODIFIER
