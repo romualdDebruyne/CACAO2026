29dd11f93493231864c44348ce0d42aa59eb244a
unknown <matteo.touzet@imt-atlantique.net>
2026-04-21 10:01:23 +0200

    Matteo TOUZET: Correction transformateur 4

--- src/abstraction/eq7Transformateur4/Transformateur4.java
+++ src/abstraction/eq7Transformateur4/Transformateur4.java
@@ -1,11 +1,6 @@
 package abstraction.eq7Transformateur4;
 
-<<<<<<< HEAD
-
-public class Transformateur4 extends Transformateur4Production {
-=======
 public class Transformateur4 extends Transformateur4Vente {
->>>>>>> 86f0cbd5dc5cf967a9eddea2385c3a4e9537f721
 	
 	public Transformateur4() {
 		super();
